7caa297ee0b6fafb10722a6d6d579259aa424c03
Pierre-Tremblay <76471947+Pierre-Tremblay@users.noreply.github.com>
2022-06-27 12:45:48 +0200

    first commit v2

--- src/main/java/fr/diginamic/Auteurs.java
+++ src/main/java/fr/diginamic/Auteurs.java
@@ -1,8 +1,60 @@
 package fr.diginamic;
 
-import javax.persistence.EntityManager;
+import javax.persistence.*;
 
-public class Auteur {
+@Entity
+@Table(name = "Auteur")
+public class Auteurs {
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Auteurs{");
+        sb.append("id=").append(id);
+        sb.append(", nom='").append(nom).append('\'');
+        sb.append(", prenom='").append(prenom).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
+    @Column(name = "Nom")
+    private String nom;
+
+    @Column(name = "Prenom")
+    private String prenom;
+
+    public Auteurs() {
+    }
+
+    public Auteurs(int id, String nom, String prenom) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
 }
--- src/main/java/fr/diginamic/ConnexionJpa.java
+++ src/main/java/fr/diginamic/ConnexionJpa.java
@@ -1,2 +1,30 @@
-package fr.diginamic;public class ConnexionJpa {
+package fr.diginamic;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ConnexionJpa {
+    public static void main(String[] args) {
+
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa-connect");
+        EntityManager em = entityManagerFactory.createEntityManager();
+        Auteurs auteur = em.find(Auteurs.class, 245);
+        if (auteur != null) {
+            System.out.println(auteur);
+        }
+
+        Auteurs auteur1 = new Auteurs();
+        auteur1.setId(245);
+        auteur1.setNom("Papincourt");
+        auteur1.setPrenom("Marc-Henri");
+
+        em.getTransaction().begin();
+        em.merge(auteur1);
+        em.getTransaction().commit();
+
+
+    }
 }
+
